631bed3f45b64b3fae86d03bb115ca7b40513486
piercegwang <piercegwang@users.noreply.github.com>
2020-08-20 23:38:43 -0700

    Updated help page, added $help_admin

--- src/main.py
+++ src/main.py
@@ -42,16 +42,24 @@ async def on_ready():
 @bot.command()
 async def help(ctx):
     embed=discord.Embed(title="Help", description="Practicing! We all have to do it... so why not make it fun? Using this bot, we aim to make practicing a social and encouraging experience. To get started, simply join one of the designated practicing channels!", color=0x3c7f61)
-    embed.set_author(name="Heifetz Practice Bot")
+    embed.set_author(name="Discord Practice Bot")
     embed.add_field(name="$practice", value="If no one is practicing yet, use $practice to start practicing.", inline=False)
     embed.add_field(name="$excuse", value="Unmutes a user so they can talk / give you feedback. Usage: $excuse", inline=False)
     embed.add_field(name="$unexcuse", value="Mutes a user once they are done giving feedback.", inline=False)
     embed.add_field(name="$stop", value="If you don't want to practice anymore, you can use $stop to tell the bot that you are done practicing.", inline=False)
     embed.add_field(name="$song", value="If you are practicing, you can use this command to set the song you are practicing. Usage: $song <song name>", inline=False)
     embed.add_field(name="$np", value="This command will tell you information about the user practicing.", inline=False)
-    embed.set_footer(text="This bot was made by Pierce Wang for the Heifetz Discord. Message will disappear in 20 seconds.")
+    embed.set_footer(text="This bot was made by Pierce. Message will disappear in 20 seconds.")
+    await ctx.send(embed=embed, delete_after = 20.0)
+
+@bot.command()
+async def help_admin(ctx):
+    embed=discord.Embed(title="Help", description="Server admin command guide.", color=0x3c7f61)
+    embed.set_author(name="Heifetz Practice Bot")
+    embed.add_field(name="$add_practice_room", value="Add a practice room. Each practice room must be linked to a text channel. Usage: $add_practice_room voice_channel_id text_channel_id", inline=False)
+    embed.add_field(name="$remove_practice_room", value="Remove a practice room. Usage: $remove_practice_room voice_channel_id", inline=False)
+    embed.set_footer(text="More to come! Message will disappear in 20 seconds.")
     await ctx.send(embed=embed, delete_after = 20.0)
 
 bot.load_extension("practice")
-
 bot.run(os.environ['BOT_TOKEN'])
